4b64f8bfff7dd7aadd0bedd3f321dcd7feea143b
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-02 15:30:42 -0500

    complete

--- main.js
+++ main.js
@@ -46,18 +46,27 @@ const makeExtinct = function(dinosaur) {
 }
 
 const isCarnivore = function(dinosaur) {
+  return dinosaur['carnivore'] ? true : false
 }
 
 const isExtinct = function(dinosaur) {
+  return dinosaur['extinct'] ? true : false
+
 }
 
 const isTriassic = function(dinosaur) {
+  return dinosaur['period'] === 'Triassic'? true : false
+
 }
 
 const isJurassic = function(dinosaur) {
+  return dinosaur['period'] === 'Jurassic'? true : false
+
 }
 
 const isCretaceous = function(dinosaur) {
+  return dinosaur['period'] === 'Cretaceous'? true : false
+
 }
 
 
